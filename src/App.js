1b390f92f43493883a143849c8a22628ff555fb4
Dan Kiss <flowfast47@gmail.com>
2019-05-09 16:36:38 -0700

    add jsx expressions and statements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>The app component</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const name = "Larry";
+    const showHello = true;
+    const showMath = true;
+    const numb1 = 40;
+    const numb2 = 23;
+
+    let math;
+    if (showMath) {
+      math = (
+        <h4>
+          {numb1} + {numb2} = {numb1 + numb2}
+        </h4>
+      );
+    } else {
+      math = null;
+    }
+
+    return (
+      <div className="App">
+        <h1>The app component</h1>
+        {showHello ? <h4>Hello {name.toUpperCase()}</h4> : null}
+        {math}
+      </div>
+    );
+  }
 }
 
 export default App;
