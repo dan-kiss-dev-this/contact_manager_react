f91e4978b6bdf04b2a2766d294d488fc177ba4e7
Dan Kiss <flowfast47@gmail.com>
2019-05-10 13:57:57 -0700

    bind onShowClick in constructor

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,12 +3,25 @@ import PropTypes from "prop-types";
 // import "./contact.css";
 
 class Contact extends Component {
+  constructor() {
+    super();
+    this.state = {};
+
+    this.onShowClick = this.onShowClick.bind(this);
+  }
+  // state = {};
+  onShowClick() {
+    console.log(this.state);
+  }
+
   render() {
     const { contact } = this.props;
     const { name, email, phone } = contact;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
+        <h4>
+          {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
+        </h4>
         <ul className="list-group">
           <li className="list-group-item">Email {email}</li>
           <li className="list-group-item">Phone {phone}</li>
