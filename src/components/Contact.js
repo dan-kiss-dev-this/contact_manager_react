d142d24d5ca011bd0c7c7b98ef22811a8873aa8c
Dan Kiss <flowfast47@gmail.com>
2019-05-10 14:57:40 -0700

    refactor how components work with state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,24 +3,35 @@ import PropTypes from "prop-types";
 // import "./contact.css";
 
 class Contact extends Component {
-  state = {};
-
-  onShowClick = e => {
-    console.log(e.target);
+  state = {
+    showContactInfo: false
   };
+
+  // onShowClick = e => {
+  //   this.setState({ showContactInfo: !this.state.showContactInfo });
+  // };
 
   render() {
     const { contact } = this.props;
     const { name, email, phone } = contact;
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
         <h4>
-          {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
+          {name}{" "}
+          <i
+            onClick={() =>
+              this.setState({ showContactInfo: !this.state.showContactInfo })
+            }
+            className="fas fa-sort-down"
+          />
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email {email}</li>
-          <li className="list-group-item">Phone {phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email {email}</li>
+            <li className="list-group-item">Phone {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
