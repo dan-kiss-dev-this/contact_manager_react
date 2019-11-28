84d5f69c4c772588b16adaec2e05f75591c5168f
Dan Kiss <flowfast47@gmail.com>
2019-11-28 13:27:16 -0800

    Updates

---
+++ precache-manifest.f847187ececcf33510a1b7af2d58b67c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d373a1a990b733d003f145ddb85383c",
+    "url": "/contact_manager_react/index.html"
+  },
+  {
+    "revision": "08df9ff862cc88746e8b",
+    "url": "/contact_manager_react/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "752a6c2adfede446119e",
+    "url": "/contact_manager_react/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "08df9ff862cc88746e8b",
+    "url": "/contact_manager_react/static/js/2.20571da1.chunk.js"
+  },
+  {
+    "revision": "752a6c2adfede446119e",
+    "url": "/contact_manager_react/static/js/main.10d37bc3.chunk.js"
+  },
+  {
+    "revision": "826866c234e74579a79c",
+    "url": "/contact_manager_react/static/js/runtime~main.6fb8c003.js"
+  }
+]);
