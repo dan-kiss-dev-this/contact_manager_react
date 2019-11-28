b897adec7bfc25566f5dc413aaf9bf34423cb428
Dan Kiss <flowfast47@gmail.com>
2019-11-28 13:05:24 -0800

    add put request for updates

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/layout/Header";
 import About from "./components/pages/About";
 import { Provider } from "./context";
 import AddContact from "./components/contacts/AddContact";
+import EditContact from './components/contacts/EditContact'
 import NotFound from "./components/pages/NotFound";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -22,6 +23,7 @@ class App extends Component {
                 <Route exact path="/contact/add" component={AddContact} />
                 <Route exact path="/about/" component={About} />
                 <Route exact path="/about/:id" component={About} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
                 <Route component={NotFound} />
               </Switch>
             </div>
